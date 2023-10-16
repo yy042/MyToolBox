fca4f4f5b8b1dbc43e938ed71a8a309d435ecf3b
yy042 <1030322547@qq.com>
2023-10-16 16:37:02 +0800

    Fixed the problem about failing in loading images in the first fragment in the viewPager2.

--- app/src/main/java/cn/edu/fzu/mytoolbox/adapter/FeedPicAreaAdapter.kt
+++ app/src/main/java/cn/edu/fzu/mytoolbox/adapter/FeedPicAreaAdapter.kt
@@ -1,13 +1,22 @@
 package cn.edu.fzu.mytoolbox.adapter
 
+import android.os.Handler
+import android.widget.ImageView
 import cn.edu.fzu.mytoolbox.R
 import com.bumptech.glide.Glide
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 
-class FeedPicAreaAdapter(layoutResId: Int, data: MutableList<String>) : BaseQuickAdapter<String, BaseViewHolder>(layoutResId, data) {
+class FeedPicAreaAdapter(layoutResId: Int,
+                         data: MutableList<String>) :
+    BaseQuickAdapter<String, BaseViewHolder>(layoutResId, data) {
     override fun convert(holder: BaseViewHolder, item: String) {
-        Glide.with(context).load(item).into(holder.getView(R.id.ivFeedPicOne))
+        // 在图片加载之前添加一些延迟，以确保视图已被正确初始化
+        // 为了解决第1个fragment个别item图片来不及加载的情况：其他fragment在切换到它们时，可能有更多的时间用于图片加载，因此它们的图片可以正常显示。
+        val view = holder.getView<ImageView>(R.id.ivFeedPicOne)
+        view.postDelayed({
+            Glide.with(context).load(item).into(view)
+        }, 200)
     }
 
     override fun getItemCount(): Int {
